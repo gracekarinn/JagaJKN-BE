b863eba1cc2fbbdbebb81e0f6c1c562c3168e836
gracekarinn <gracekarina12@gmail.com>
2024-10-25 22:12:07 +0700

    feat: add JWT utility functions for token generation and validation

---
+++ internal/utils/jwt.go
@@ -0,0 +1,58 @@
+package utils
+
+import (
+	"errors"
+	"jagajkn/internal/models"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/google/uuid"
+)
+
+type JWTClaims struct {
+	UserID string `json:"user_id"`
+	NIK    string `json:"nik"`
+	jwt.RegisteredClaims
+}
+
+func GenerateID() string {
+    return uuid.New().String()
+}
+
+func GenerateToken(user *models.User, secretKey string) (string, error) {
+	claims := JWTClaims{
+		UserID: user.ID,
+		NIK:    user.NIK,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString([]byte(secretKey))
+	if err != nil {
+		return "", err
+	}
+
+	return signedToken, nil
+}
+
+func ValidateToken(tokenString string, secretKey string) (*JWTClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &JWTClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("invalid signing method")
+		}
+		return []byte(secretKey), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(*JWTClaims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, errors.New("invalid token")
+}
