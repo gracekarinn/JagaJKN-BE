b12769d97d9294c2336d0f2568a5999ed680a48b
gracekarinn <gracekarina12@gmail.com>
2024-10-26 20:43:20 +0700

    refactor: update RecordRepository to include FindByUserNIK and FindByNoSEP methods

--- internal/repository/rekam_repository.go
+++ internal/repository/rekam_repository.go
@@ -7,35 +7,32 @@ import (
 )
 
 type RecordRepository struct {
-	db *gorm.DB
+    db *gorm.DB
 }
 
 func NewRecordRepository(db *gorm.DB) *RecordRepository {
-	return &RecordRepository{db: db}
+    return &RecordRepository{db: db}
 }
 
 func (r *RecordRepository) Create(record *models.RecordKesehatan) error {
-	return r.db.Create(record).Error
+    if err := r.db.Create(record).Error; err != nil {
+        return err
+    }
+    return nil
 }
 
-func (r *RecordRepository) FindByID(id string) (*models.RecordKesehatan, error) {
-	var record models.RecordKesehatan
-	err := r.db.Preload("User").Where("id = ?", id).First(&record).Error
-	if err != nil {
-		return nil, err
-	}
-	return &record, nil
+func (r *RecordRepository) FindByUserNIK(userNIK string) ([]models.RecordKesehatan, error) {
+    var records []models.RecordKesehatan
+    if err := r.db.Where("user_nik = ?", userNIK).Find(&records).Error; err != nil {
+        return nil, err
+    }
+    return records, nil
 }
 
-func (r *RecordRepository) FindByUserID(userID string) ([]models.RecordKesehatan, error) {
-	var records []models.RecordKesehatan
-	err := r.db.Where("user_id = ?", userID).Find(&records).Error
-	if err != nil {
-		return nil, err
-	}
-	return records, nil
-}
-
-func (r *RecordRepository) Update(record *models.RecordKesehatan) error {
-	return r.db.Save(record).Error
+func (r *RecordRepository) FindByNoSEP(noSEP string) (*models.RecordKesehatan, error) {
+    var record models.RecordKesehatan
+    if err := r.db.Where("no_sep = ?", noSEP).First(&record).Error; err != nil {
+        return nil, err
+    }
+    return &record, nil
 }
