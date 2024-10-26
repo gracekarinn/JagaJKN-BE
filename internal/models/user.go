10cbf11f62f033ccc5582f3404580646ae1a8cdd
gracekarinn <gracekarina12@gmail.com>
2024-10-26 21:22:52 +0700

    refactor: remove unused field and update User model primary key

--- internal/models/user.go
+++ internal/models/user.go
@@ -3,7 +3,6 @@ package models
 import (
 	"time"
 
-	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
@@ -20,7 +19,6 @@ const (
 	Kelas3 KelasBPJS = "KELAS_3"
 )
 
-// UserSignupInput represents the data needed for user registration
 type UserSignupInput struct {
 	NIK         string  `json:"nik" binding:"required,len=16"`
 	NamaLengkap string  `json:"namaLengkap" binding:"required"`
@@ -29,22 +27,18 @@ type UserSignupInput struct {
 	Password    string  `json:"password" binding:"required,min=6"`
 }
 
-// UserLoginInput represents the data needed for user login
 type UserLoginInput struct {
 	NIK      string `json:"nik" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
 
-// User model for authentication and basic info
 type User struct {
 	NIK 	   string     `gorm:"type:varchar(16);primaryKey" json:"nik"`
-	ID          string     `gorm:"primaryKey" json:"id"`
 	CreatedAt   time.Time  `json:"createdAt"`
 	NamaLengkap string     `gorm:"type:varchar(255);not null" json:"namaLengkap"`
 	NoTelp      string     `gorm:"type:varchar(20);not null" json:"noTelp"`
 	Email       *string    `gorm:"type:varchar(255);uniqueIndex" json:"email,omitempty"`
 	Password    string     `gorm:"type:varchar(255);not null" json:"-"`
-	// Fields from JKN
 	NoKartuJKN     *string    `gorm:"type:varchar(13);uniqueIndex" json:"noKartuJKN,omitempty"`
 	JenisKelamin   *Gender    `gorm:"type:gender" json:"jenisKelamin,omitempty"`
 	TanggalLahir   *time.Time `gorm:"type:date" json:"tanggalLahir,omitempty"`
@@ -54,13 +48,12 @@ type User struct {
 }
 
 func (u *User) BeforeCreate(tx *gorm.DB) error {
-	u.ID = uuid.New().String()
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
-	if err != nil {
-		return err
-	}
-	u.Password = string(hashedPassword)
-	return nil
+    hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+    if err != nil {
+        return err
+    }
+    u.Password = string(hashedPassword)
+    return nil
 }
 
 func (u *User) CheckPassword(password string) error {
