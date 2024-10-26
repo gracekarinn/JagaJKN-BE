ac3d13562aa334c5f9272604944931b4c655bb82
gracekarinn <gracekarina12@gmail.com>
2024-10-26 20:43:25 +0700

    refactor: update user_repository.go to include bcrypt password hashing and JWT generation functions

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -1,46 +1,47 @@
 package repository
 
 import (
-	"jagajkn/internal/models"
+	"time"
 
+	"github.com/golang-jwt/jwt/v4"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
+
+	"jagajkn/internal/models"
 )
 
 type UserRepository struct {
-	db *gorm.DB
+    db *gorm.DB
 }
 
 func NewUserRepository(db *gorm.DB) *UserRepository {
-	return &UserRepository{db: db}
+    return &UserRepository{db: db}
 }
 
 func (r *UserRepository) Create(user *models.User) error {
-	return r.db.Create(user).Error
+    return r.db.Create(user).Error
 }
 
 func (r *UserRepository) FindByNIK(nik string) (*models.User, error) {
-	var user models.User
-	err := r.db.Where("nik = ?", nik).First(&user).Error
-	if err != nil {
-		return nil, err
-	}
-	return &user, nil
+    var user models.User
+    err := r.db.Where("nik = ?", nik).First(&user).Error
+    if err != nil {
+        return nil, err
+    }
+    return &user, nil
 }
 
-func (r *UserRepository) FindByID(id string) (*models.User, error) {
-	var user models.User
-	err := r.db.Where("id = ?", id).First(&user).Error
-	if err != nil {
-		return nil, err
-	}
-	return &user, nil
+func CheckPasswordHash(password, hash string) bool {
+    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+    return err == nil
 }
 
-func (r *RecordRepository) GetLastRecord(userID string) (*models.RecordKesehatan, error) {
-	var record models.RecordKesehatan
-	err := r.db.Where("user_id = ?", userID).Order("created_at desc").First(&record).Error
-	if err != nil {
-		return nil, err
-	}
-	return &record, nil
+func GenerateJWT(nik string, secret string) (string, error) {
+    claims := jwt.MapClaims{
+        "user_nik": nik,
+        "exp":      time.Now().Add(time.Hour * 72).Unix(),
+    }
+
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    return token.SignedString([]byte(secret))
 }
