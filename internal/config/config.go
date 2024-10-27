40e97f2abcc7e028da80ad8335e9e9552571ef89
gracekarinn <gracekarina12@gmail.com>
2024-10-27 20:59:44 +0700

    refactor: update config loading function and add BlockchainConfig struct

---
+++ internal/config/config.go
@@ -0,0 +1,86 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type Config struct {
+    DatabaseURL     string
+    ServerPort      string
+    JWTSecret       string
+    AllowedOrigin   string
+    BlockchainConfig *BlockchainConfig
+}
+
+type BlockchainConfig struct {
+    ProviderURL     string
+    PrivateKey      string
+    ContractAddress string
+}
+
+func LoadConfig() (*Config, error) {
+    if err := godotenv.Load(); err != nil {
+        return nil, fmt.Errorf("error loading .env file: %v", err)
+    }
+
+    config := &Config{
+        DatabaseURL:    getEnvOrPanic("DATABASE_URL"),
+        ServerPort:     getEnvOrDefault("SERVER_PORT", "8080"),
+        JWTSecret:      getEnvOrPanic("JWT_SECRET"),
+        AllowedOrigin:  getEnvOrDefault("ALLOWED_ORIGIN", "*"),
+        BlockchainConfig: &BlockchainConfig{
+            ProviderURL:     getEnvOrPanic("BLOCKCHAIN_PROVIDER"),
+            PrivateKey:      getEnvOrPanic("BLOCKCHAIN_PRIVATE_KEY"),
+            ContractAddress: os.Getenv("CONTRACT_ADDRESS"),
+        },
+    }
+    
+    return config, nil
+}
+
+func (c *Config) ConnectDB() (*gorm.DB, error) {
+    db, err := gorm.Open(postgres.Open(c.DatabaseURL), &gorm.Config{
+        Logger: logger.Default.LogMode(logger.Info),
+    })
+    if err != nil {
+        return nil, fmt.Errorf("error connecting to database: %v", err)
+    }
+
+    sqlDB, err := db.DB()
+    if err != nil {
+        return nil, err
+    }
+
+    if err := sqlDB.Ping(); err != nil {
+        return nil, err
+    }
+
+    sqlDB.SetMaxIdleConns(10)
+    sqlDB.SetMaxOpenConns(100)
+    return db, nil
+}
+
+func (c *Config) GetBlockchainConfig() *BlockchainConfig {
+    return c.BlockchainConfig
+}
+
+func getEnvOrPanic(key string) string {
+    value := os.Getenv(key)
+    if value == "" {
+        panic(fmt.Sprintf("Environment variable %s is required", key))
+    }
+    return value
+}
+
+func getEnvOrDefault(key, defaultValue string) string {
+    if value := os.Getenv(key); value != "" {
+        return value
+    }
+    return defaultValue
+}
