cb00065d8b2f3c580dd6f4955b2bd611658fd47e
gracekarinn <gracekarina12@gmail.com>
2024-10-27 20:59:11 +0700

    refactor: update main.go to use new config loading function

--- cmd/blockchain_test/main.go
+++ cmd/blockchain_test/main.go
@@ -1,32 +1,31 @@
 package main
 
 import (
-	"log"
-
 	"jagajkn/internal/blockchain/service"
 	"jagajkn/internal/config"
+	"log"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
-    // Load .env file
     if err := godotenv.Load(); err != nil {
         log.Fatal("Error loading .env file")
     }
 
-    cfg := config.NewBlockchainConfig()
-    
-    // Buat blockchain service
-    blockchainService, err := service.NewBlockchainService(cfg)
+    cfg, err := config.LoadConfig()
+    if err != nil {
+        log.Fatalf("Failed to load config: %v", err)
+    }
+
+    blockchainService, err := service.NewBlockchainService(cfg.GetBlockchainConfig())
     if err != nil {
         log.Fatalf("Failed to create blockchain service: %v", err)
     }
-    
-    // Test koneksi
+
     if err := blockchainService.TestConnection(); err != nil {
         log.Fatalf("Failed to connect to blockchain: %v", err)
     }
-    
+
     log.Println("Successfully connected to blockchain!")
 }
