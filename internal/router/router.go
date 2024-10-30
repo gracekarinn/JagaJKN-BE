98a46d5916f5bbffd277838cde9c0e5d420344a4
gracekarinn <gracekarina12@gmail.com>
2024-10-31 00:13:12 +0700

    refactor: update CORS configuration to allow only specific origin

--- internal/router/router.go
+++ internal/router/router.go
@@ -18,7 +18,7 @@ func SetupRouter(db *gorm.DB, cfg *config.Config, blockchainSvc *bService.Blockc
 
 
     r.Use(cors.New(cors.Config{
-        AllowOrigins:     []string{"*"},
+        AllowOrigins:     []string{"https://jagajkn-be-production.up.railway.app/"},
         AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
         AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
         ExposeHeaders:    []string{"Content-Length"},
