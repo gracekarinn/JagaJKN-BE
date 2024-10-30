a74c243ad50cd96f054780a78203bb29b47bb583
gracekarinn <gracekarina12@gmail.com>
2024-10-30 22:38:47 +0700

    refactor: update hardhat.config.js with sepolia network configuration

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: "https://rpc.ankr.com/eth_sepolia",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    hardhat: {
+      chainId: 1337,
+    },
+  },
+};
